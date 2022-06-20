acd717d708c245ed8ef94659d9efd42b1b3bb29d
zai <98795942+zainuddinhazin@users.noreply.github.com>
2022-06-20 17:47:44 +0800

    Add files via upload

--- src/gameData.js
+++ src/gameData.js
@@ -1,4 +1,44 @@
-﻿gameData = {"Terms":[{"Id":"39dbb038-790d-4106-3f57-86772bc1cef4","Name":"larynx","Definition":"Another name for this is \"voice box\"."},{"Id":"0b49005d-7afe-47b3-8f21-358457236645","Name":"circulation","Definition":"The process where blood moves throughout the body."},{"Id":"41f96e3b-4c99-46ff-82a2-42387c7a941a","Name":"epidermis","Definition":"The layer of skin on the outside of the body."},{"Id":"5f4ab8f9-574a-475e-4a67-ba811c539850","Name":"tendon","Definition":"The tissue that connects bones with muscle."},{"Id":"7cbab7b0-61d2-4dc7-6d61-b0b106230904","Name":"pulse","Definition":"The throbbing of arteries in the body as blood is pumped through them."},{"Id":"0d5e949a-14f2-45d5-5023-947cc28a0e9e","Name":"ligament","Definition":"Tissue that holds joints together."},{"Id":"c08d94f7-f62f-4f82-e1e4-c41c8d6113ff","Name":"pupil","Definition":"The opening in the eye that light passes through."}]};
-gameData.Title = "Lab Escape";
-gameData.Description = "Escape the laboratory by completing all the crosswords."; 
+﻿gameData = {
+  Terms: [
+    {
+      Id: "39dbb038-790d-4106-3f57-86772bc1cef4",
+      Name: "larynx",
+      Definition: 'Another name for this is "voice box".',
+    },
+    {
+      Id: "0b49005d-7afe-47b3-8f21-358457236645",
+      Name: "circulation",
+      Definition: "The process where blood moves throughout the body.",
+    },
+    {
+      Id: "41f96e3b-4c99-46ff-82a2-42387c7a941a",
+      Name: "epidermis",
+      Definition: "The layer of skin on the outside of the body.",
+    },
+    {
+      Id: "5f4ab8f9-574a-475e-4a67-ba811c539850",
+      Name: "tendon",
+      Definition: "The tissue that connects bones with muscle.",
+    },
+    {
+      Id: "7cbab7b0-61d2-4dc7-6d61-b0b106230904",
+      Name: "pulse",
+      Definition:
+        "The throbbing of arteries in the body as blood is pumped through them.",
+    },
+    {
+      Id: "0d5e949a-14f2-45d5-5023-947cc28a0e9e",
+      Name: "ligament",
+      Definition: "Tissue that holds joints together.",
+    },
+    {
+      Id: "c08d94f7-f62f-4f82-e1e4-c41c8d6113ff",
+      Name: "pupil",
+      Definition: "The opening in the eye that light passes through.",
+    },
+  ],
+};
+gameData.Title = "Lab Escape - Crossword Puzzle";
+gameData.Description =
+  "Escape the laboratory by completing all the crosswords.";
 gameData.imageUrls = {};
